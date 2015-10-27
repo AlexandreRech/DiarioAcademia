608a8245f957fa23f8f28cfe25a9d398872aab38
Wesley Lemos <anisan_wesley@live.com>
2015-10-27 11:37:31 -0200

    Implementada Autenticação da API Basica
    Também ajustadas algumas referencias

--- NDDigital.DiarioAcademia.WebApiFull/App_Start/OAuthConfig.cs
+++ NDDigital.DiarioAcademia.WebApiFull/App_Start/OAuthConfig.cs
@@ -3,6 +3,9 @@ using Microsoft.Owin.Security.OAuth;
 using Owin;
 using NDDigital.DiarioAcademia.WebApiFull.Providers;
 using System;
+using NDDigital.DiarioAcademia.Infraestrutura.Security.Contexts;
+using NDDigital.DiarioAcademia.Infraestrutura.Security.Repositories;
+using NDDigital.DiarioAcademia.WebApi.Providers;
 
 namespace NDDigital.DiarioAcademia.WebApiFull
 {
@@ -10,13 +13,18 @@ namespace NDDigital.DiarioAcademia.WebApiFull
     {
         public static void ConfigureOAuth(IAppBuilder app)
         {
+            // Configure the db context and user manager to use a single instance per request
+            app.CreatePerOwinContext(AuthContext.Create);
+            app.CreatePerOwinContext<UserRepository>(UserRepository.Create);
+
+
             var oAuthServerOptions = new OAuthAuthorizationServerOptions
             {
-
                 AllowInsecureHttp = true,
-                TokenEndpointPath = new PathString("/token"),
-                AccessTokenExpireTimeSpan = TimeSpan.FromDays(30),
-                Provider = new SimpleAuthorizationServerProvider()
+                TokenEndpointPath = new PathString("/oauth/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new CustomOAuthProvider(),
+                AccessTokenFormat = new CustomJwtFormat("http://localhost:31648")
             };
 
             // Token Generation

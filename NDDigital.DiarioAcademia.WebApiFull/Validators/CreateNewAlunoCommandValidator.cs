66d4ec1acfeb192c38abc5716c934fb394590a7f
Thiago Sartor <tt.sartor@hotmail.com>
2015-10-30 09:41:05 -0200

    Criando os validators na API.

---
+++ NDDigital.DiarioAcademia.WebApiFull/Validators/CreateNewAlunoCommandValidator.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using NDDigital.DiarioAcademia.Aplicacao.DTOs;
+
+namespace NDDigital.DiarioAcademia.WebApiFull.Validators
+{
+    public class CreateNewAlunoCommandValidator : AbstractValidator<AlunoDTO>
+    {
+        public CreateNewAlunoCommandValidator()
+        {
+            RuleFor(x => x.Descricao)
+                .NotEmpty()
+                .NotNull();
+
+            RuleFor(x => x.TurmaId)
+                .NotEmpty()
+                .NotNull();
+
+            RuleFor(x => x.Bairro)
+                 .NotEmpty()
+                .NotNull();
+
+            RuleFor(x => x.Localidade)
+                .NotEmpty()
+                .NotNull();
+
+            RuleFor(x => x.Uf)
+          .NotEmpty()
+          .NotNull();
+        }
+    }
+}
---
+++ NDDigital.DiarioAcademia.WebApiFull/Validators/CreateNewTurmaCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using NDDigital.DiarioAcademia.Aplicacao.DTOs;
+
+namespace NDDigital.DiarioAcademia.WebApiFull.Validators
+{
+    public class CreateNewTurmaCommandValidator : AbstractValidator<TurmaDTO>
+    {
+        public CreateNewTurmaCommandValidator()
+        {
+            RuleFor(x => x.Descricao)
+                .NotEmpty()
+                .NotNull();
+
+            RuleFor(x => x.Ano)
+                 .NotEmpty()
+                .NotNull()
+                .When(x => x.Ano > 2000);
+        }
+    }
+}

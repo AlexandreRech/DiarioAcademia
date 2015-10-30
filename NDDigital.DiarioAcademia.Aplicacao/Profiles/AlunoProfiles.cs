92ef5f3e27eb7b1126b58bbb2fb8533d15f22918
Wesley Lemos <anisan_wesley@live.com>
2015-10-30 14:00:42 -0200

    Implementaç]ão inicial mappers

---
+++ NDDigital.DiarioAcademia.Aplicacao/Profiles/AlunoProfiles.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using NDDigital.DiarioAcademia.Aplicacao.DTOs;
+using NDDigital.DiarioAcademia.Dominio.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NDDigital.DiarioAcademia.Aplicacao.Profiles
+{
+   public class AlunoProfiles:Profile
+    {
+
+
+        protected override void Configure()
+        {
+            Mapper.CreateMap<Aluno, AlunoDTO>();
+
+
+
+        }
+
+
+    }
+}

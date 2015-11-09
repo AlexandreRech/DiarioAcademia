0f5dc7b363fe855c20312e82a27153739bd12ef1
Guitoniello <guitoniello@gmail.com>
2015-11-09 17:47:33 -0200

    Correção falta de arquivo

---
+++ NDDigital.DiarioAcademia.Apresentacao.AngleTry/src/common/translate/translate.constants.js
@@ -0,0 +1,15 @@
+﻿(function () {
+
+    angular
+        .module("app.translate")
+        .value('resource', {
+            SAVED_SUCCESSFUL: "",
+            DELETED_SUCCESSFUL: "",
+            UNAVAILABLE_SERVER: "",
+            SUCCESS_REQUEST: "",
+            STUDENT_FOUNDED: "",
+            STUDENT_EDITED: "",
+            WELCOME: ""
+
+        });
+})();
